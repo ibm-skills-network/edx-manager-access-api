f3af455220a65be3381519887f97bdacc2626e84
Michael Lin <michael.lin1@ibm.com>
2021-03-23 23:24:14 -0600

    fix: remove service account creation

--- edx_manager_access_api/settings/common.py
+++ edx_manager_access_api/settings/common.py
@@ -4,8 +4,4 @@ def plugin_settings(settings):
     """
     Modify the provided settings object with settings specific to this plugin.
     """
-    settings.AUTH_USERNAME = os.environ.get('AUTH_USERNAME')
-    settings.AUTH_PASSWORD = os.environ.get('AUTH_PASSWORD')
-
-    if not settings.AUTH_USERNAME or not settings.AUTH_PASSWORD:
-        raise Exception('AUTH_USERNAME or AUTH_PASSWORD environment variables not set')
+    pass
--- edx_manager_access_api/urls.py
+++ edx_manager_access_api/urls.py
@@ -8,13 +8,3 @@ urlpatterns = [
     url(r'^grant_access$', views.GrantManagerAccessView.as_view(), name='grant_manager_access'),
     url(r'^revoke_access$', views.RevokeManagerAccessView.as_view(), name='revoke_manager_access'),
 ]
-
-# Since urls.py is executed once, create service user here for server to server auth
-from django.conf import settings
-from django.contrib.auth.models import User
-try:
-    User.objects.get(username=settings.AUTH_USERNAME)
-except User.DoesNotExist:
-    User.objects.create_user(username=settings.AUTH_USERNAME,
-                                    email=settings.EMAIL,
-                                    password=settings.AUTH_PASSWORD, is_staff=True)
