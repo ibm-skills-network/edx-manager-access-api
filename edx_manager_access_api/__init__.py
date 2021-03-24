42e7f3e4bbe128cbd33ff5e07b786ffd38590fb8
Michael Lin <michael.lin1@ibm.com>
2021-03-23 23:04:26 -0600

    chore: bump version

--- edx_manager_access_api/__init__.py
+++ edx_manager_access_api/__init__.py
@@ -2,6 +2,6 @@
 API to enable and disable manager access to Studio and Django on portals.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 default_app_config = 'edx_manager_access_api.apps.EdxManagerAccessApiConfig'  # pylint: disable=invalid-name
