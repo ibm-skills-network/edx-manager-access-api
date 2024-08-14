ccf1148adc47e3ce2f75eec8bf1fea21960f3f61
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2024-08-14 15:21:04 -0400

    chore(urls): updated django url syntax

--- edx_manager_access_api/urls.py
+++ edx_manager_access_api/urls.py
@@ -1,10 +1,10 @@
 """
 URLs for edx_manager_access_api.
 """
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^grant_access$', views.GrantManagerAccessView.as_view(), name='grant_manager_access'),
-    url(r'^revoke_access$', views.RevokeManagerAccessView.as_view(), name='revoke_manager_access'),
+    path('grant_access', views.GrantManagerAccessView.as_view(), name='grant_manager_access'),
+    path('revoke_access', views.RevokeManagerAccessView.as_view(), name='revoke_manager_access'),
 ]
